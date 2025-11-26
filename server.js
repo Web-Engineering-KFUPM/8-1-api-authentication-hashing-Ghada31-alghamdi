116a3d650abd820b7c9154fd4f936a0736171be3
Ghada31-alghamdi <s202269300@kfupm.edu.sa>
2025-11-26 11:32:01 +0300

    TODO1

--- server.js
+++ server.js
@@ -256,6 +256,26 @@ app.get("/", (_req, res) => {
 // =========================
 app.post("/register", async (req, res) => {
   // Implement logic here based on the TODO 1.
+  try{
+  const { email, password } = req.body || {};
+  if(!email || !password){
+    return res.status(400).json({ error: "Email and password are required" });
+  }
+  else{
+    const existing = users.find((u) => u.email === email);
+    if(!existing){
+      return res.status(400).json({ error: "User already exists" });
+    }
+    else{
+      const hash = await bcrypt.hash(password, 10);
+      users.push({ email, passwordHash: hash });
+      return res.status(201).json({ message: "User registered!" });
+    }
+  }}
+  catch(error){
+    console.error("Register error:", err)       
+    return res.status(500).json({ error: "Server error during register" });
+  }
 });
 
 // =========================
